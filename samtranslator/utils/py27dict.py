f133bbbbf94d5eb1d757f736de83d6a4b283c599
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2021-07-14 14:14:12 -0700

    Update Py27Keys and Py27Dict to replicate native Python2 dict behavior

--- samtranslator/utils/py27dict.py
+++ samtranslator/utils/py27dict.py
@@ -1,5 +1,7 @@
 import ctypes
 import copy
+import json
+import sys
 import logging
 
 
@@ -12,36 +14,32 @@ from six import string_types
 LOG = logging.getLogger(__name__)
 
 
-class Py27Str(str):
+class Py27UniStr(str):
     """
-    A str subclass to 
+    A str subclass to allow string be recognized as Py27 unicode string by Py27Dict
     """
     def __add__(self, other):
-        if isinstance(other, Py27Str):
-            return Py27Str(super(Py27Str, self).__add__(other)) 
-        return super(Py27Str, self).__add__(other)
+        return Py27UniStr(super(Py27UniStr, self).__add__(other))
 
-    def __mul__(self, n):
-        return Py27Str(super(Py27Str, self).__mul__(n))
-
-    def format(self, *args, **kwargs):
-        """
-        """
-        return Py27Str(super(Py27Str, self).format(*args, **kwargs))
-
-    def join(self, iterable):
-        joined = super(Py27Str, self).join(iterable)
-        if all(isinstance(item, Py27Str) for item in iterable):
-            return Py27Str(joined)
-        return joined
+    def __repr__(self):
+        if sys.version_info.major >= 3:
+            return super(Py27UniStr, self).__repr__()
+        return "u" + super(Py27UniStr, self).__repr__() + ""
 
     def upper(self):
-        return Py27Str(super(Py27Str, self).upper())
+        return Py27UniStr(super(Py27UniStr, self).upper())
 
+    def lower(self):
+        return Py27UniStr(super(Py27UniStr, self).lower())
 
 class Py27Keys(Keys):
     """
     A subclass of Keys from py27hash
+    Difference from Keys - In Keys, hashes are only calculated and cached when keys() method is
+    invoked. This is fine if the dictionary is not manipulated at all between creation and keys()
+    is invoked for the first time. However, in Swagger Editor, the swagger body is manipulated
+    (adding/removing keys & values) without calling keys(). 
+    Therefore, when adding
     The main change is to keep a cache of calculated hash id for existing keys
     """
     def __init__(self):
@@ -55,40 +53,61 @@ class Py27Keys(Keys):
         if key in self.keylist:
             # Remove key from list
             self.keylist.remove(key)
-            del self.keyhash[key]
+            if key in self.keyhash:
+                del self.keyhash[key]
             # Clear cached keys
             self.keysort = None
 
+    def add(self, key):
+        """
+        Calculate hash of the new key every
+        """
+        if key and key not in self.keylist:
+            # Append key to list
+            self.keylist.append(key)
+
+            # Update keysort
+            self._update_keysort()
+
+            # Resize dict if 2/3 capacity
+            if len(self.keylist) * 3 >= ((self.mask + 1) * 2):
+                # Reset key list to simulate the dict resize + copy operation
+                self.keylist = self.keys()
+                self.keysort = None
+
+                self.setMask()
+
     def keys(self):
         if not self.keysort:
-            keys = []
-            hids = set(self.keyhash.values())
-
-            for k in self.keylist:
-                if k in self.keyhash:
-                    hid = self.keyhash[k]
-                else:
-                    # C API uses unsigned values
-                    h = ctypes.c_size_t(Hash.hash(k)).value
-                    i = h & self.mask
-
-                    hid = i
-                    perturb = h
-
-                    while hid in hids:
-                        i = (i << 2) + i + perturb + 1
-                        hid = i & self.mask
-                        perturb >>= Keys.PERTURB_SHIFT
-
-                self.keyhash[k] = hid
-                keys.append((hid, k))
-                hids.add(hid)
-
-            # Cache result - performance - clear if more keys added
-            self.keysort = [v for (k, v) in sorted(keys, key=lambda x: x[0])]
-
+            self._update_keysort()
         return self.keysort
 
+    def _update_keysort(self):
+        keys = []
+        hids = set(self.keyhash.values())
+
+        for k in self.keylist:
+            if k in self.keyhash:
+                hid = self.keyhash[k]
+            else:
+                # C API uses unsigned values
+                h = ctypes.c_size_t(Hash.hash(k)).value
+                i = h & self.mask
+
+                hid = i
+                perturb = h
+
+                while hid in hids:
+                    i = (i << 2) + i + perturb + 1
+                    hid = i & self.mask
+                    perturb >>= Keys.PERTURB_SHIFT
+
+            self.keyhash[k] = hid
+            keys.append((hid, k))
+            hids.add(hid)
+
+        # Cache result - performance - clear if more keys added
+        self.keysort = [v for (k, v) in sorted(keys, key=lambda x: x[0])]
     
 
 class Py27Dict(Dict):
@@ -108,12 +127,12 @@ class Py27Dict(Dict):
 
         for x, k in enumerate(self):
             string += ", " if x > 0 else ""
-            if not isinstance(k, Py27Str):
+            if isinstance(k, Py27UniStr):
                 string += "u"
             string += "'%s': " % k
 
             if isinstance(self[k], string_types):
-                if not isinstance(self[k], Py27Str):
+                if isinstance(self[k], Py27UniStr):
                     string += "u"
                 string += "'%s'" % self[k]
             else:
@@ -123,9 +142,16 @@ class Py27Dict(Dict):
 
         return string
 
+    def setdefault(self, __key, __default):
+        if __key not in self:
+            self[__key] = __default
+        return self[__key]
+
+
 def to_py27dict(original_dict):
     if isinstance(original_dict, dict):
         py27_dict = Py27Dict(copy.deepcopy(original_dict))
+        # py27_dict = Py27Dict(**original_dict)
         for k in py27_dict.keys():
             py27_dict[k] = to_py27dict(py27_dict[k])
         return py27_dict
@@ -133,3 +159,17 @@ def to_py27dict(original_dict):
         return [to_py27dict(item) for item in original_dict]
     else:
         return original_dict
+
+
+def _object_pairs_hook(pairs):
+    original_dict = {
+        Py27UniStr(k): Py27UniStr(v) if isinstance(v, string_types) else v
+        for k, v in pairs
+    }
+    return dict(**original_dict)
+
+def mark_unicode_str_in_template(template_dict):
+    """
+    In Python2, 
+    """
+    return json.loads(json.dumps(template_dict), object_pairs_hook=_object_pairs_hook)
